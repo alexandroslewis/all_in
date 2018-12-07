446f6806315d62abaa9282c5a098d2f39ae80611
Morgan Griffiths <C5ipo7i@yahoo.com>
2018-12-07 11:13:23 -0800

    removed extra parenths

--- table_checker.js
+++ table_checker.js
@@ -10,7 +10,7 @@ var callback = function(mutationsList, observer) {
     for(var mutation of mutationsList) {
         if(mutation.addedNodes.length>0){
             var table = mutation.addedNodes[0].textContent;
-            if(table.indexOf("Omaha") > 0 || (table.indexOf("Holdem") > 0)){
+            if(table.indexOf("Omaha") > 0 || table.indexOf("Holdem") > 0){
                 checkTable(table);
             }
         }
@@ -21,6 +21,30 @@ var observer = new MutationObserver(callback);
 
 init ();
 
+// function checkTable(table) {
+//     console.log('checkTables')
+//     console.log('OmahaHiLo5')
+//     console.log(table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5 Card")>0)
+//     console.log('OmahaHiLo4')
+//     console.log(table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5 Card")<0)
+//     console.log('OmahaHi5')
+//     console.log(table.indexOf("Omaha")>0 && table.indexOf("5 Card")>0)
+//     console.log('OmahaHi4')
+//     console.log(table.indexOf("Omaha")>0)
+//     console.log('Holdem')
+//     console.log(table.indexOf("Holdem")>0)
+//     if(table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5 Card")>0){
+//         chrome.runtime.sendMessage("runScript");
+//     } else if(table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5 Card")<0){
+//         chrome.runtime.sendMessage("runScript");
+//     } else if(table.indexOf("Omaha")>0 && table.indexOf("5 Card")>0){
+//         chrome.runtime.sendMessage("runScript");
+//     } else if(table.indexOf("Omaha")>0){
+//         chrome.runtime.sendMessage("runScript");
+//     } else if(table.indexOf("Holdem")>0){
+//         chrome.runtime.sendMessage("runScript");
+//     }
+// }
 function checkTable(table) {
     console.log('checkTables')
     console.log('OmahaHiLo5')
@@ -33,50 +57,26 @@ function checkTable(table) {
     console.log(table.indexOf("Omaha")>0)
     console.log('Holdem')
     console.log(table.indexOf("Holdem")>0)
-    if(table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5 Card")>0){
-        chrome.runtime.sendMessage("runScript");
-    } else if(table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5 Card")<0){
-        chrome.runtime.sendMessage("runScript");
-    } else if(table.indexOf("Omaha")>0 && table.indexOf("5 Card")>0){
-        chrome.runtime.sendMessage("runScript");
-    } else if(table.indexOf("Omaha")>0){
-        chrome.runtime.sendMessage("runScript");
-    } else if(table.indexOf("Holdem")>0){
-        chrome.runtime.sendMessage("runScript");
-    }
+    chrome.storage.sync.get(["settings"], function(result) {
+        if(result.settings){
+            var settings = result.settings;
+            for(var i=0;i<settings.length;i++){
+                if(settings[i].enabled == true){
+                    if(settings[i].option == 1 && table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5 Card")>0){
+                        chrome.runtime.sendMessage("runScript");
+                    } else if(settings[i].option == 2 && table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5 Card")<0){
+                        chrome.runtime.sendMessage("runScript");
+                    } else if(settings[i].option == 3 && table.indexOf("Omaha")>0 && table.indexOf("5 Card")<0){
+                        chrome.runtime.sendMessage("runScript");
+                    } else if(settings[i].option == 4 && table.indexOf("Omaha")>0){
+                        chrome.runtime.sendMessage("runScript");
+                    } else if(settings[i].option == 5 && table.indexOf("Holdem")>0){
+                        chrome.runtime.sendMessage("runScript");
+                    }
+                }
+            }
+        } else {
+            console.err("Extension settings have not been saved.")
+        }
+    });
 }
-// function checkTable(table) {
-//     console.log('checkTables')
-//     console.log('OmahaHiLo5')
-//     console.log(table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5")>0)
-//     console.log('OmahaHiLo4')
-//     console.log(table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5")<0)
-//     console.log('OmahaHi5')
-//     console.log(table.indexOf("Omaha")>0 && table.indexOf("5")>0)
-//     console.log('OmahaHi4')
-//     console.log(table.indexOf("Omaha")>0)
-//     console.log('Holdem')
-//     console.log(table.indexOf("Holdem")>0)
-//     chrome.storage.sync.get(["settings"], function(result) {
-//         if(result.settings){
-//             var settings = result.settings;
-//             for(var i=0;i<settings.length;i++){
-//                 if(settings[i].enabled == true){
-//                     if(settings[i].option == 1 && table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5")>0){
-//                         chrome.runtime.sendMessage("runScript");
-//                     } else if(settings[i].option == 2 && table.indexOf("Omaha")>0 && table.indexOf("Hi-Lo")>0 && table.indexOf("5")<0){
-//                         chrome.runtime.sendMessage("runScript");
-//                     } else if(settings[i].option == 3 && table.indexOf("Omaha")>0 && table.indexOf("5")<0){
-//                         chrome.runtime.sendMessage("runScript");
-//                     } else if(settings[i].option == 4 && table.indexOf("Omaha")>0){
-//                         chrome.runtime.sendMessage("runScript");
-//                     } else if(settings[i].option == 5 && table.indexOf("Holdem")>0){
-//                         chrome.runtime.sendMessage("runScript");
-//                     }
-//                 }
-//             }
-//         } else {
-//             console.err("Extension settings have not been saved.")
-//         }
-//     });
-// }
