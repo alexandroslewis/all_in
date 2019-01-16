f539a5781d764c94f4aaff330994a91db2a622d8
Morgan <morgan@Anastasias-MacBook-Pro.local>
2019-01-15 17:10:32 -0800

    Changed the way we account for changes in the dom. Now we listen for player card changes instead of community card changes. This has some additional effects in that if the allin is uncalled it can cause the second allin to be unobserved. I have added an additional observer that is targeting that circumstance. But have not tested it (testing === death). Also updated simulation for totCCards == 7 to account for the double runs properly. GOOD DAY SIR

--- all_in.js
+++ all_in.js
@@ -1,96 +1,262 @@
-var config = {attributes: true, childList: true, characterData: true};
+var config = {
+  attributes: true,
+  childList: true,
+  characterData: true
+};
+
+var subtreeConfig = {
+  attributes: true,
+  childList: true,
+  characterData: true,
+  subtree: true
+};
+
+var subnodeConfig = {
+  childList: true,
+  subtree: true
+};
+
+var ALLIN = false;
+var PLAYEROBS = false;
+
+// The idea is:
+// First check for allins. Once allin is discovered.
+// We monitor for non yellow backs. If real cards are present there is a showdown.
+// Then we reset
+// Potentially need to add the player cards to the community cards
 
 var callback = function(mutationsList, observer) {
-    for(var mutation of mutationsList) {
-        console.log(mutation.target.id)
-        if (mutation.target.className == "player_name" && mutation.target.innerText == 'ALL IN') {
-            console.log("first player went all in");
-            observer.disconnect();
-            var cards = document.getElementById("community_cards");
-            observer.observe(cards, config);
-        } else if (mutation.target.className != "player_name" && mutation.target.childElementCount > 0) {
-            console.log("game is all in");
-            var assets = document.getElementsByClassName("card");
-            getCards(assets);
-        } else if (mutation.target.className != "player_name" && mutation.target.childElementCount == 0) {
-            console.log("resetting script");
-            observer.disconnect();
-            instantiateObservers();
+  for (var mutation of mutationsList) {
+    if (
+      mutation.target.className == "player_name" &&
+      mutation.target.innerText == "ALL IN"
+    ) {
+      console.log("Player went all in");
+      observer.disconnect();
+      ALLIN = true;
+      PLAYEROBS = false;
+      var playerIndCards = document.getElementsByClassName("noselect")[5][
+        "childNodes"
+      ][3]["children"];
+      for (var i = 0; i < playerIndCards.length; i++) {
+        observer.observe(playerIndCards[i], config);
+      }
+      playerNode = document.getElementById("player_cards");
+      observer.observe(playerNode, subtreeConfig);
+    } else if (mutation.target.className != "player_cards" && ALLIN == true) {
+      // Check for yellow backs in player_cards. If yellow backs present then pass.
+      // Else game is over and grab all cards
+      var playerCards = document.getElementsByClassName("noselect")[5][
+        "childNodes"
+      ][3]["children"];
+      let gameOver = true;
+      if (!playerCards.length && PLAYEROBS == false) {
+        //This only happens when ALLin player is uncalled and game has reset
+        console.log("no length resetting script");
+        observer.disconnect();
+        instantiateObservers();
+      } else if (playerCards.length > 0) {
+        console.log("node triggered");
+        // Only need to check first card
+        for (var i = 0; i < playerCards.length; i++) {
+          if (playerCards[i].alt == "X") {
+            gameOver = false;
+            break;
+          }
         }
+        if (gameOver == true) {
+          //Game is over
+          console.log("first card");
+          console.log(playerCards[0]);
+          console.log("game is all in");
+          //grab hands and community cards
+          var playerHands = document.getElementsByClassName("card");
+          console.log("playerhands");
+          console.log(playerHands);
+          // var Ccards = getCommunityCards(
+          //   document.getElementById("community_cards")
+          // );
+          // console.log("community cards");
+          // console.log(Ccards);
+          getCards(playerHands);
+          console.log("resetting script");
+          ALLIN = false;
+          observer.disconnect();
+          instantiateObservers();
+        }
+      }
     }
-};   
+  }
+};
 
 var observer = new MutationObserver(callback);
 
 function instantiateObservers() {
-    console.log("working...");
-    var playerNames = document.getElementsByClassName("player_name");
-    for(var i=0;i<playerNames.length;i++){
-        observer.observe(playerNames[i], config);
-    }
+  console.log("working...");
+  PLAYEROBS = true;
+  var playerNames = document.getElementsByClassName("player_name");
+  for (var i = 0; i < playerNames.length; i++) {
+    observer.observe(playerNames[i], config);
+  }
 }
 
 instantiateObservers();
 
-function getCards(cards) {
-    var assetsArr = [];
-    for(var i=0;i<cards.length;i++){
-        var imgUrl = cards[i].src
-        var imgUrlSpl = imgUrl.split("/")
-        var card = imgUrlSpl[imgUrlSpl.length-1];
-        assetsArr.push(card);
-        if(i == cards.length-1){
-            var cardsArr = checkDictionary(assetsArr);
-            saveSim(cardsArr);
-        }
+function getCommunityCards(cards) {
+  var communityCards = [];
+  for (var i = 0; i < cards.length; i++) {
+    if (cards[i].style["display"] == "block") {
+      var card = cards[i].alt;
+      communityCards.push(card);
     }
+  }
+  return communityCards;
+}
+
+function getCards(cards) {
+  var assetsArr = [];
+  for (var i = 0; i < cards.length; i++) {
+    // have to check whether the card is displayed or not
+    if (cards[i].style["display"] == "block") {
+      var imgUrl = cards[i].src;
+      var imgUrlSpl = imgUrl.split("/");
+      var card = imgUrlSpl[imgUrlSpl.length - 1];
+      assetsArr.push(card);
+      if (i == cards.length - 1) {
+        console.log("prior to dictionary");
+        console.log(assetsArr);
+        var cardsArr = checkDictionary(assetsArr);
+        saveSim(cardsArr);
+      }
+    }
+  }
 }
 
 function checkDictionary(cards) {
-    var cardValues = [];
-    var dictionary = { "0.png":"2c", "1.png":"2d", "2.png":"2h", "3.png":"2s", "4.png":"3c", "5.png":"3d", "6.png":"3h", "7.png":"3s", "8.png":"4c", "9.png":"4d", "10.png":"4h", "11.png":"4s", "12.png":"5c", "13.png":"5d", "14.png":"5h", "15.png":"5s", "16.png":"6c", "17.png":"6d", "18.png":"6h", "19.png":"6s", "20.png":"7c", "21.png":"7d", "22.png":"7h", "23.png":"7s", "24.png":"8c", "25.png":"8d", "26.png":"8h", "27.png":"8s", "28.png":"9c", "29.png":"9d", "30.png":"9h", "31.png":"9s", "32.png":"Tc", "33.png":"Td", "34.png":"Th", "35.png":"Ts", "36.png":"Jc", "37.png":"Jd", "38.png":"Jh", "39.png":"Js", "40.png":"Qc", "41.png":"Qd", "42.png":"Qh", "43.png":"Qs", "44.png":"Kc", "45.png":"Kd", "46.png":"Kh", "47.png":"Ks", "48.png":"Ac", "49.png":"Ad", "50.png":"Ah", "51.png":"As" };
-    for(var i=0;i<cards.length;i++){
-        var cardValue = dictionary[cards[i]];
-        cardValues.push(cardValue);
-        if(i==cards.length-1){
-            return cardValues;
-        }
+  var cardValues = [];
+  var dictionary = {
+    "0.png": "2c",
+    "1.png": "2d",
+    "2.png": "2h",
+    "3.png": "2s",
+    "4.png": "3c",
+    "5.png": "3d",
+    "6.png": "3h",
+    "7.png": "3s",
+    "8.png": "4c",
+    "9.png": "4d",
+    "10.png": "4h",
+    "11.png": "4s",
+    "12.png": "5c",
+    "13.png": "5d",
+    "14.png": "5h",
+    "15.png": "5s",
+    "16.png": "6c",
+    "17.png": "6d",
+    "18.png": "6h",
+    "19.png": "6s",
+    "20.png": "7c",
+    "21.png": "7d",
+    "22.png": "7h",
+    "23.png": "7s",
+    "24.png": "8c",
+    "25.png": "8d",
+    "26.png": "8h",
+    "27.png": "8s",
+    "28.png": "9c",
+    "29.png": "9d",
+    "30.png": "9h",
+    "31.png": "9s",
+    "32.png": "Tc",
+    "33.png": "Td",
+    "34.png": "Th",
+    "35.png": "Ts",
+    "36.png": "Jc",
+    "37.png": "Jd",
+    "38.png": "Jh",
+    "39.png": "Js",
+    "40.png": "Qc",
+    "41.png": "Qd",
+    "42.png": "Qh",
+    "43.png": "Qs",
+    "44.png": "Kc",
+    "45.png": "Kd",
+    "46.png": "Kh",
+    "47.png": "Ks",
+    "48.png": "Ac",
+    "49.png": "Ad",
+    "50.png": "Ah",
+    "51.png": "As"
+  };
+  for (var i = 0; i < cards.length; i++) {
+    var cardValue = dictionary[cards[i]];
+    cardValues.push(cardValue);
+    if (i == cards.length - 1) {
+      return cardValues;
     }
+  }
 }
 
 function getTableId() {
-    var url = location.href;
-    var temp = url.split(":");
-    var id = temp[2];
-    return id;
+  var url = location.href;
+  var temp = url.split(":");
+  var id = temp[2];
+  return id;
 }
 
 function getGame() {
-    console.log('getGame')
-    var info = document.getElementsByClassName("table_info")[0].textContent;
-    if(info.indexOf("Omaha")>0 && info.indexOf("Hi-Lo")>0 && info.indexOf("5 Card")>0){
-        return "OmahaHiLo5";
-    } else if(info.indexOf("Omaha")>0 && info.indexOf("Hi-Lo")>0 && info.indexOf("5 Card")<0) {
-        return "OmahaHiLo";
-    } else if(info.indexOf("Omaha")>0 && info.indexOf("5 Card")>0 && info.indexOf("Hi-Lo")<0) {
-        return "OmahaHi5";
-    } else if(info.indexOf("Omaha")>0 && info.indexOf("5 Card")<0 && info.indexOf("Hi-Lo")<0) {
-        return "OmahaHi";
-    } else if(info.indexOf("Holdem")>0) {
-        return "Holdem";
-    }
+  console.log("getGame");
+  var info = document.getElementsByClassName("table_info")[0].textContent;
+  if (
+    info.indexOf("Omaha") > 0 &&
+    info.indexOf("Hi-Lo") > 0 &&
+    info.indexOf("5 Card") > 0
+  ) {
+    return "OmahaHiLo5";
+  } else if (
+    info.indexOf("Omaha") > 0 &&
+    info.indexOf("Hi-Lo") > 0 &&
+    info.indexOf("5 Card") < 0
+  ) {
+    return "OmahaHiLo";
+  } else if (
+    info.indexOf("Omaha") > 0 &&
+    info.indexOf("5 Card") > 0 &&
+    info.indexOf("Hi-Lo") < 0
+  ) {
+    return "OmahaHi5";
+  } else if (
+    info.indexOf("Omaha") > 0 &&
+    info.indexOf("5 Card") < 0 &&
+    info.indexOf("Hi-Lo") < 0
+  ) {
+    return "OmahaHi";
+  } else if (info.indexOf("Holdem") > 0) {
+    return "Holdem";
+  }
 }
 
 function getCommunityCardsSize() {
-    communityCards = document.getElementById("community_cards").childNodes;
-    return communityCards.length;
+  let communityCards = document.getElementById("community_cards")["children"];
+  let count = 0;
+  for (var i = 0; i < communityCards.length; i++) {
+    if (communityCards[i].style["display"] == "block") {
+      count++;
+    }
+  }
+  console.log("community card length");
+  console.log(count);
+  return count;
 }
 
-function saveSim(cards){
-    var tableId = getTableId();
-    var game = getGame();
-    var totCCards = getCommunityCardsSize();
-    console.log('saveSim')
-    console.log(game)
-    chrome.storage.local.set({"key":{"id":tableId,"game":game,"cards":cards, "totCCards":totCCards}});
+function saveSim(cards) {
+  var tableId = getTableId();
+  var game = getGame();
+  var totCCards = getCommunityCardsSize();
+  console.log("saveSim");
+  console.log(game);
+  console.log(cards);
+  chrome.storage.local.set({
+    key: { id: tableId, game: game, cards: cards, totCCards: totCCards }
+  });
 }
--- simulation.js
+++ simulation.js
@@ -1,111 +1,115 @@
 chrome.storage.local.get(["key"], function(result) {
-    clearSimulation();
-    selectGame(result.key.game, result.key.cards, result.key.totCCards);
+  clearSimulation();
+  selectGame(result.key.game, result.key.cards, result.key.totCCards);
 });
 
 function clearSimulation() {
-    var clear = document.querySelector('[title="Clear all inputs"]');
-    clear.click();
+  var clear = document.querySelector('[title="Clear all inputs"]');
+  clear.click();
 }
 
 function selectGame(game, cards, totCCards) {
-    console.log('selectGame')
-    console.log(game)
-    var numHands;
-    if(game=="OmahaHiLo5"){
-        document.getElementById("gameSelector").value = "o85";
-        numHands = (cards.length - totCCards)/5;
-        parseCards(cards, 5, totCCards, numHands);
-        toggleHands(numHands);
-    } else if (game=="OmahaHiLo"){
-        document.getElementById("gameSelector").value = "o8";
-        numHands = (cards.length - totCCards)/4;
-        parseCards(cards, 4, totCCards, numHands);
-        toggleHands(numHands);
-    } else if (game=="OmahaHi5"){
-        document.getElementById("gameSelector").value = "oh5";
-        numHands = (cards.length - totCCards)/5;
-        parseCards(cards, 5, totCCards, numHands);
-        toggleHands(numHands);
-    } else if (game=="OmahaHi"){
-        document.getElementById("gameSelector").value = "oh";
-        numHands = (cards.length - totCCards)/4;
-        parseCards(cards, 4, totCCards, numHands);
-        toggleHands(numHands);
-    } else if (game=="Razz"){
-        document.getElementById("gameSelector").value = "rz";
-        numHands = (cards.length - totCCards)/7;
-        parseCards(cards, 7, totCCards, numHands);
-        toggleHands(numHands);
-    } else if (game=="StudHi"){
-        document.getElementById("gameSelector").value = "st";
-        numHands = (cards.length - totCCards)/7;
-        parseCards(cards, 7, totCCards, numHands);
-        toggleHands(numHands);
-    } else if (game=="StudHiLo"){
-        document.getElementById("gameSelector").value = "s8";
-        numHands = (cards.length - totCCards)/7;
-        parseCards(cards, 7, totCCards, numHands);
-        toggleHands(numHands);
-    } else if (game=="Holdem"){
-        document.getElementById("gameSelector").value = "he";
-        numHands = (cards.length - totCCards)/2;
-        parseCards(cards, 2, totCCards, numHands);
-        toggleHands(numHands);
-    }
+  console.log("selectGame");
+  console.log(game);
+  var numHands;
+  if (game == "OmahaHiLo5") {
+    document.getElementById("gameSelector").value = "o85";
+    numHands = (cards.length - totCCards) / 5;
+    parseCards(cards, 5, totCCards, numHands);
+    toggleHands(numHands);
+  } else if (game == "OmahaHiLo") {
+    document.getElementById("gameSelector").value = "o8";
+    numHands = (cards.length - totCCards) / 4;
+    parseCards(cards, 4, totCCards, numHands);
+    toggleHands(numHands);
+  } else if (game == "OmahaHi5") {
+    document.getElementById("gameSelector").value = "oh5";
+    numHands = (cards.length - totCCards) / 5;
+    parseCards(cards, 5, totCCards, numHands);
+    toggleHands(numHands);
+  } else if (game == "OmahaHi") {
+    document.getElementById("gameSelector").value = "oh";
+    numHands = (cards.length - totCCards) / 4;
+    parseCards(cards, 4, totCCards, numHands);
+    toggleHands(numHands);
+  } else if (game == "Razz") {
+    document.getElementById("gameSelector").value = "rz";
+    numHands = (cards.length - totCCards) / 7;
+    parseCards(cards, 7, totCCards, numHands);
+    toggleHands(numHands);
+  } else if (game == "StudHi") {
+    document.getElementById("gameSelector").value = "st";
+    numHands = (cards.length - totCCards) / 7;
+    parseCards(cards, 7, totCCards, numHands);
+    toggleHands(numHands);
+  } else if (game == "StudHiLo") {
+    document.getElementById("gameSelector").value = "s8";
+    numHands = (cards.length - totCCards) / 7;
+    parseCards(cards, 7, totCCards, numHands);
+    toggleHands(numHands);
+  } else if (game == "Holdem") {
+    document.getElementById("gameSelector").value = "he";
+    numHands = (cards.length - totCCards) / 2;
+    parseCards(cards, 2, totCCards, numHands);
+    toggleHands(numHands);
+  }
 }
 
 function toggleHands(hands) {
-    var toggle = document.getElementById("handToggler");
-    if(hands>2 && toggle.innerText == "More hands"){
-        toggle.click();
-    } else if(hands==2 && toggle.innerText == "Fewer hands"){
-        toggle.click();
-    }
+  var toggle = document.getElementById("handToggler");
+  if (hands > 2 && toggle.innerText == "More hands") {
+    toggle.click();
+  } else if (hands == 2 && toggle.innerText == "Fewer hands") {
+    toggle.click();
+  }
 }
 
 function parseCards(cards, handSize, totCCards, numHands) {
-    var hands = [];
-    var boards = [];
-    var pcards = cards.slice(totCCards,cards.length);
-    var ccards = cards.slice(0,totCCards);
-    boards.push(ccards.slice(0,3));
-    if(totCCards==7){
-        boards.push(ccards.slice(0,4));
-        ccards.splice(3,1);
-        boards.push(ccards.slice(0,4));
-    } else if(totCCards==10){
-        boards.push(ccards.slice(0,4), ccards.slice(5,8), ccards.slice(5,9));
-    } else {
-        boards.push(ccards.slice(0,4));
+  var hands = [];
+  var boards = [];
+  var pcards = cards.slice(totCCards, cards.length);
+  var ccards = cards.slice(0, totCCards);
+  boards.push(ccards.slice(0, 3));
+  //If totCCards==6 its the same as ==5 because its two diff rivers
+  if (totCCards == 7) {
+    boards.push(ccards.slice(0, 4));
+    ccards.splice(3, 2);
+    boards.push(ccards.slice(0, 4));
+  } else if (totCCards == 10) {
+    boards.push(
+      ccards.slice(0, 3),
+      ccards.slice(0, 4),
+      ccards.slice(5, 8),
+      ccards.slice(5, 9)
+    );
+  } else {
+    boards.push(ccards.slice(0, 4));
+  }
+  for (var i = 0; i < pcards.length; i + handSize) {
+    var hand = pcards.splice(i, i + handSize);
+    hands.push(hand);
+    if (hands.length == numHands) {
+      createSimulation(boards, hands);
     }
-    for(var i=0;i<pcards.length;i+handSize){
-        var hand = pcards.splice(i,i+handSize);
-        hands.push(hand);
-        if(hands.length==numHands){
-            createSimulation(boards, hands);
-        }
-    } 
+  }
 }
 
 function createSimulation(boards, hands) {
-    chrome.storage.local.clear();
-    for(var i=0;i<boards.length;i++){
-        clearSimulation();
-        document.getElementById("boardField").value = boards[i].join("");
-        submitSimulation(hands);
-    }
+  chrome.storage.local.clear();
+  for (var i = 0; i < boards.length; i++) {
+    clearSimulation();
+    document.getElementById("boardField").value = boards[i].join("");
+    submitSimulation(hands);
+  }
 }
 
 function submitSimulation(hands) {
-    for(var i=0;i<hands.length;i++){
-        var hIn = "h" + (i+1);
-        document.getElementById(hIn).value = hands[i].join("");
-        if(i == (hands.length-1)){
-            var submit = document.querySelector('[title="Simulate an all-in race"]');
-            submit.click();
-        }
+  for (var i = 0; i < hands.length; i++) {
+    var hIn = "h" + (i + 1);
+    document.getElementById(hIn).value = hands[i].join("");
+    if (i == hands.length - 1) {
+      var submit = document.querySelector('[title="Simulate an all-in race"]');
+      submit.click();
     }
+  }
 }
-
-
